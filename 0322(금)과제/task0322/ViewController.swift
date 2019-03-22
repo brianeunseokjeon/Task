acfd9e7542f51bd9e2c31a1420ee282f95935b8d
brianeunseokjeon <qed327@naver.com>
2019-03-22 19:26:40 +0900

    task

---
+++ 0322(금)과제/task0322/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  task0322
+//
+//  Created by brian은석 on 22/03/2019.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var switch2: UISwitch!
+    
+    @IBOutlet weak var segname: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func switching(_ sender: UISwitch) {
+        if sender.isOn {
+            label.text = "on"
+        } else {
+            label.text = "off"
+        }
+    }
+    @IBAction func segChange(_ sender: UISegmentedControl) {
+        label.text = segname.titleForSegment(at: segname.selectedSegmentIndex) ?? ""
+    }
+}
+
+
+
+
+
