122e41bbf62bc6869a2f56f1230b21a94317fafc
brianeunseokjeon <qed327@naver.com>
2019-06-14 17:20:30 +0900

    6월 14일 알고리즘5회차

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -202,3 +202,30 @@ let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 for (index, value) in upperCase.enumerated() {
     
 }
+
+// 6월 14일 (금) 5회차
+
+//문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+//
+//제한 사항
+//문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+//첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+
+func solution14(_ s:String) -> String {
+    var arr: [String] = []
+    var str = ""
+    var count = 0
+    for x in s {
+        count += 1
+        if count % 2 == 1 {
+            arr.append("\(x.uppercased())")
+        } else {
+            arr.append("\(x.lowercased())")
+        }
+    }
+    for y in arr {
+        str += y
+    }
+    return str
+}
+solution14("try hello world")
